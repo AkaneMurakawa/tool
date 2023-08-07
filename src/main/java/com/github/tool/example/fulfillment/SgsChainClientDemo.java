52f6ceef5f02e79fc7bf1d677ea59568ddd60745
chenshaoj <614784818@qq.com>
2023-08-07 20:31:15 +0800

    sgs chain sdk demo

--- src/main/java/com/github/tool/example/fulfillment/SgsChainClientDemo.java
+++ src/main/java/com/github/tool/example/fulfillment/SgsChainClientDemo.java
@@ -12,7 +12,9 @@ public class SgsChainClientDemo {
      */
     public static void main(String[] args) {
         SgsChainClient client = new SgsChainClient(new SgsChainProperties());
-        getWarehouse(client);
+        // getWarehouse(client);
+        getProduct(client);
+        getInventory(client);
     }
 
     public static void getWarehouse(SgsChainClient client) {
@@ -23,5 +25,23 @@ public class SgsChainClientDemo {
                 "}";
         String apiMethod = "cdm.api.basics.warehouse.getWarehouseService";
         client.request(Method.POST, apiMethod, ApiTypeEnum.CDM_API.getType(), data);
+    }
+
+    public static void getProduct(SgsChainClient client) {
+        //language=JSON
+        String data = "{\n" +
+                "\"page\": \"1\",\n" +
+                "\"pageSize\": \"50\",\n" +
+                "\"status\": \"02\"\n" +
+                "}";
+        String apiMethod = "cdm.api.basics.product.getCdmProductService";
+        client.request(Method.POST, apiMethod, ApiTypeEnum.CDM_API.getType(), data);
+    }
+
+    public static void getInventory(SgsChainClient client) {
+        //language=JSON
+        String data = "{\"productNos\":[\"OT-LMLZBY-1578-US-1\"]}";
+        String apiMethod = "oms.api.warehouse.storage.getInventory";
+        client.request(Method.POST, apiMethod, ApiTypeEnum.OMS_API.getType(), data);
     }
 }
